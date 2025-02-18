d2c53aded457fcb9ebb39fe22bfb1de3aaf6bc3d
Ankith15 <ankith15bhandary@gmail.com>
2025-02-18 18:41:11 +0530

    solved error

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import os
 import streamlit as st
 from tensorflow.keras.models import load_model
@@ -13,44 +12,30 @@ load_dotenv()
 fun = os.getenv('anki')
 print(fun)
 
-@st.cache_resource
-def load_trained_model():
-    model = load_model(r'F:\\machine learning\\Rice_leaf_Disease\Disease_dtct (1).h5')
-    return model
+model = load_model('Disease_dtct (1).h5')
 
+class_labels = ["Rice_Blast", "Bacterial_leaf_blight", "Healthy_leaf", "Tungro"]
 
-# fun_id = f"runs:/{fun}/model"
+st.title("Rice Plant Disease Detection")
+st.write("Upload rice plant leaf image")
 
-model =load_model(r'https://github.com/Ankith15/Rice_leaf_disease/blob/main/Disease_dtct%20(1).h5')
-
-
-class_labels = ["Rice_Blast","Bacterial_leaf_blight", "Healthy_leaf", "Tungro"]
-
-st.title(" Rice Plant Disease Detection")
-st.write("upload rice plant leaf image")
-
-
-upload_file = st.file_uploader("choose image from Device",type = ['jpg', 'png', 'jpeg', 'bmp'])
-
+upload_file = st.file_uploader("Choose image from device", type=['jpg', 'png', 'jpeg', 'bmp'])
 
 if upload_file is not None:
-
     image_data = Image.open(upload_file)
-    st.image(image_data,caption= "uploaded Image",use_column_width = True)
+    st.image(image_data, caption="Uploaded Image", use_column_width=True)
 
     def preprocess_image(img):
         img = img.resize((224, 224), Image.Resampling.LANCZOS)
         img_arr = image.img_to_array(img)
-        img_arr = np.expand_dims(img_arr,axis = 0)
-        img_arr = img_arr/255.0
+        img_arr = np.expand_dims(img_arr, axis=0)
+        img_arr = img_arr / 255.0
         return img_arr
 
     processed_image = preprocess_image(image_data)
-
-    
 
     predictions = model.predict(processed_image)
     predicted_class = class_labels[np.argmax(predictions)]
 
     st.write(f"Predicted class: {predicted_class}")
-    st.write(f"confidence : {np.max(predictions)*100:.2f}%")
+    st.write(f"Confidence: {np.max(predictions)*100:.2f}%")
